7b5a2aa31ec8c8d62e0f6fb1f0ef28f6853cb86b
Jane Cui <jane.anqi.cui@gmail.com>
2023-10-07 16:26:39 -0400

    add custom styling

--- app.js
+++ app.js
@@ -14,8 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(cookieParser());
 app.use(checkAuth);
-app.use(express.static('public'));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('/public'));
 
 const post = require('./controllers/posts')(app);
 require('./controllers/comments.js')(app);
